34a8631f8af03ec7694ad2de06cae3038262b212
apss-pohl <95089598+apss-pohl@users.noreply.github.com>
2022-07-01 09:10:01 +0200

    Initial commit

---
+++ apps/nuxt/.nuxt/dist/server/client.manifest.mjs
@@ -0,0 +1,21 @@
+export default {
+  "publicPath": "/_nuxt/",
+  "all": [
+    "@vite/client",
+    "entry.mjs",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/welcome.vue?vue&type=style&index=0&scoped=true&lang.css"
+  ],
+  "initial": [
+    "@vite/client",
+    "entry.mjs",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/error-dev.vue?vue&type=style&index=0&scoped=true&lang.css",
+    "@fs/sandbox/node_modules/@nuxt/ui-templates/dist/templates/welcome.vue?vue&type=style&index=0&scoped=true&lang.css"
+  ],
+  "async": [],
+  "modules": {}
+}
---
+++ apps/nuxt/src/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">Hello Nuxt {{ version }}!</div>
+</template>
+<script lang="ts" setup>
+import { useError } from "#app";
+const error = useError();
+console.log(error);
+console.log(12);
+</script>
+<style scoped>
+.hello {
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 3rem;
+  padding: 10rem;
+}
+</style>
+
+
